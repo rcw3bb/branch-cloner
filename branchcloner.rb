ece190cad6dff6eb6b3ecc5d466fc074dbd232c6
Ron Webb <ron.webb@fmg.co.nz>
2017-01-11 15:57:49 +1300

    Implement logging via log4r. Implement threaded logic.

--- branchcloner.rb
+++ branchcloner.rb
@@ -1,14 +1,27 @@
 require_relative 'lib/cl_args_parser'
 require_relative 'lib/config'
+
 require 'open3'
 require 'fileutils'
+require 'log4r'
+require 'log4r/MDC'
+require 'log4r/yamlconfigurator'
+require 'log4r/outputter/datefileoutputter'
+
+LOG_CONFIG = Log4r::YamlConfigurator
+LOG_CONFIG['HOME'] = '.'
+LOG_CONFIG.load_yaml_file(Pathname.new('conf/log4r.yaml'))
+
+LOG = Log4r::Logger['branchcloner']
 
 options = BranchCloner::Parser.parse ARGV
 cfg = BranchCloner::Config.new options
 
-cfg.repositories.each do |repo|
+def self.processRepo(cfg, options, repo)
   code = repo[BranchCloner::Config::ATTR_CODE]
   description = repo[BranchCloner::Config::ATTR_DESCRIPTION]
+
+  Log4r::MDC.put(:code, code)
 
   if options.clean
     work_dir = cfg.getRepoAttributeConf(code, BranchCloner::Config::ATTR_WORKING_DIR)
@@ -20,25 +33,36 @@ cfg.repositories.each do |repo|
     work_dir = %Q(#{work_dir})
 
     if work_dir!=nil && !work_dir.empty? && Dir.exist?(work_dir)
-      puts "Deleting: #{work_dir}"
+      LOG.info "Deleting: #{work_dir}"
       FileUtils.rm_rf(work_dir)
     end
   end
 
   cmd = cfg.getCommand code, options.mode, options.group
-  puts "Processing <<< #{description} >>>"
-  puts cmd
+  LOG.info "Processing <<< #{description} >>>"
+  LOG.info cmd
 
   Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
     while line = stdout.gets
-      puts line
+      LOG.debug line.chomp
     end
     exit_status = wait_thr.value
     unless exit_status.success?
-      puts "FAILED #{cmd}"
+      LOG.error "FAILED #{cmd}"
     end
   end
-  puts '-----'
+
+  LOG.info '-----'
 end
+
+thr = nil
+
+cfg.repositories.each do |repo|
+  thr = Thread.new {
+    self.processRepo cfg, options, repo
+  }
+end
+
+thr.join
 
 puts "Done"
